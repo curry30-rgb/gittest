c072c588738aba7eda701d18a6ce360fe7217b24
curry30-rgb <1627590317@qq.com>
2022-10-10 22:12:40 +0800

    test>>>

---
+++ src/main/java/com/itheima/Application.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Title: Application
+ * @Description:
+ * @Author: Fashion
+ * @Version: 1.0
+ * @create: 2022/10/10 22:11
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
