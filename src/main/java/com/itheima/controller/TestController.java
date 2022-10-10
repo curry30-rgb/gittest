cc0d86e536b1af906d8a09f3e9b345f448f02564
curry30-rgb <83053816+curry30-rgb@users.noreply.github.com>
2022-10-10 21:59:10 +0800

    Add files via upload

---
+++ src/main/java/com/itheima/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Title: TestController
+ * @Description:
+ * @Author: Fashion
+ * @Version: 1.0
+ * @create: 2022/10/10 21:48
+ */
+@RestController
+public class TestController {
+}
